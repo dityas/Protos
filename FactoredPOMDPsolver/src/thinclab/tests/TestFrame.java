46088294aaf7e904321816017e883a0d5a43718a
Aditya Shinde <shinde.aditya386@gmail.com>
2019-06-26 02:55:54 -0400

    begin creating frames for higher domains

---
+++ FactoredPOMDPsolver/src/thinclab/tests/TestFrame.java
@@ -0,0 +1,39 @@
+/*
+ * 
+ *
+ *	Author: Aditya Shinde
+ * 
+ *	email: shinde.aditya386@gmail.com
+ */
+package thinclab.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/*
+ * @author adityas
+ *
+ */
+class TestFrame {
+	
+	/*
+	 * Test parsing and solution of frames in an IPOMDP
+	 */
+	
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testL0Frames() {
+		System.out.println("Running testL0Frames()");
+	}
+
+}
