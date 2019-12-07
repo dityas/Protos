d15833a70e5aa459d4f21e6455d00e9998b8c14a
Aditya Shinde <shinde.aditya386@gmail.com>
2019-12-07 07:20:22 -0500

    Pre compute hashes for TripletSet

--- Protos/src/thinclab/legacy/TripletConfig.java
+++ Protos/src/thinclab/legacy/TripletConfig.java
@@ -1,18 +1,38 @@
 package thinclab.legacy;
 
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 class TripletConfig {
 		private DD dd1;
 		private DD dd2;
 		private int[][] config;
 
+		private int hash;
+		
 		public TripletConfig(DD dd1, DD dd2, int[][] config) {
 				this.dd1 = dd1;
 				this.dd2 = dd2;
 				this.config = config; // Config.clone(config);
+				
+				this.precomputeHash();
+		}
+		
+		private void precomputeHash() {
+			/*
+			 * Compute hash after init to avoid computing it at every call to hashCode
+			 */
+			
+			this.hash = 
+					new HashCodeBuilder()
+						.append(this.dd1.hashCode())
+						.append(this.dd2.hashCode())
+						.append(Config.hashCode(this.config))
+						.toHashCode();
 		}
 
 		public int hashCode() {
-				return dd1.getAddress() + dd2.getAddress() + Config.hashCode(config);
+//				return dd1.getAddress() + dd2.getAddress() + Config.hashCode(config);
+			return this.hash;
 		}
 
 		public boolean equals(Object obj) {
@@ -20,8 +40,8 @@ class TripletConfig {
 				if (obj.getClass() != getClass()) return false;
 				TripletConfig triplet = (TripletConfig)obj;
 
-				if (((dd1 == triplet.dd1 && dd2 == triplet.dd2) || 
-						 (dd2 == triplet.dd1 && dd1 == triplet.dd2)) && 
+				if (((dd1.equals(triplet.dd1) && dd2.equals(triplet.dd2)) || 
+						 (dd2.equals(triplet.dd1) && dd1.equals(triplet.dd2))) && 
 						Config.equals(config, triplet.config)) 
 						return true;
 				else return false;
