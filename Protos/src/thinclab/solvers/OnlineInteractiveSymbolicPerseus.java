5325b9b775bfbfcf0b73e83365767db6c31723f3
Aditya Shinde <shinde.aditya386@gmail.com>
2020-01-15 03:09:05 -0500

    Relax alpha vector convergence condition further

--- Protos/src/thinclab/solvers/OnlineInteractiveSymbolicPerseus.java
+++ Protos/src/thinclab/solvers/OnlineInteractiveSymbolicPerseus.java
@@ -375,7 +375,7 @@ public class OnlineInteractiveSymbolicPerseus extends OnlineIPBVISolver {
 				break;
 			}
 			
-			if (stepId > 10 && this.declareApproxConvergenceForAlphaVectors(
+			if (stepId > 3 && this.declareApproxConvergenceForAlphaVectors(
 					this.alphaVectors.length, numIter, beliefRegion.length)) {
 				logger.warn("DECLARING APPROXIMATE CONVERGENCE AT ERROR: " + bellmanErr
 						+ " BECAUSE ALL BELIEFS ARE BEING USED AND NUM ALPHAS IS CONSTANT");
