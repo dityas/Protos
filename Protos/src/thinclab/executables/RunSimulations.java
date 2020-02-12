2f92e722084d14d9974d596fc79aa23aeddf7046
Aditya Shinde <shinde.aditya386@gmail.com>
2020-02-12 00:53:33 -0500

    debug null pointer while solving J in multi agent sim

--- Protos/src/thinclab/executables/RunSimulations.java
+++ Protos/src/thinclab/executables/RunSimulations.java
@@ -229,6 +229,9 @@ public class RunSimulations extends Executable {
 								"plan_frame_" + T.f.frameID + "_" + i);
 					}
 					
+					/* store ref to agent J */
+					POMDP pomdp = ipomdp.lowerLevelFrames.get(frameSample);
+					
 					ipomdp.clearLowerLevelSolutions();
 					
 					/* set context back to IPOMDP */
@@ -255,7 +258,6 @@ public class RunSimulations extends Executable {
 					
 					/* Agent j */
 					LOGGER.info("Solving agent J...");
-					POMDP pomdp = ipomdp.lowerLevelFrames.get(frameSample);
 					pomdp.setGlobals();
 					OfflineSymbolicPerseus jSolver = 
 							OfflineSymbolicPerseus.createSolverWithSSGAExpansion(
