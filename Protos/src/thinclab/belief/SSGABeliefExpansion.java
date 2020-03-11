a25b3ec3c645aa6db24f2f09fbde6972cfe108fc
Aditya Shinde <shinde.aditya386@gmail.com>
2020-03-11 17:07:51 -0400

    bug fix in L1 domain

--- Protos/src/thinclab/belief/SSGABeliefExpansion.java
+++ Protos/src/thinclab/belief/SSGABeliefExpansion.java
@@ -153,9 +153,6 @@ public class SSGABeliefExpansion extends BeliefRegionExpansionStrategy {
 					else act = Global.random.nextInt(this.f.getActions().size());
 						
 					DD obsDist = this.f.getObsDist(belief, this.f.getActions().get(act));
-					LOGGER.debug("for " +  this.f.getActions().get(act) +
-							Arrays.toString(this.f.getTiForAction(this.f.getActions().get(act))));
-					LOGGER.debug("for " +  this.f.getActions().get(act));
 					
 					int[][] obsConfig = null;
 					
