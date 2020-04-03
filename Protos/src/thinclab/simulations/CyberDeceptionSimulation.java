39d00b0b43c5ac76b91f9729a97251744097c3fb
Aditya Shinde <shinde.aditya386@gmail.com>
2020-04-03 16:03:12 -0400

    change action names for debugging

--- Protos/src/thinclab/simulations/CyberDeceptionSimulation.java
+++ Protos/src/thinclab/simulations/CyberDeceptionSimulation.java
@@ -86,10 +86,10 @@ public class CyberDeceptionSimulation extends MultiAgentSimulation {
 	@Override
 	public int step(int currentNode) {
 		
-		String[] obs1 = this.getL1Observation("HELLO_FROM_DEFENDER");
+		String[] obs1 = this.getL1Observation("ACTION_FROM_DEFENDER");
 		LOGGER.debug("Defender got observation " + Arrays.toString(obs1));
 		
-		String[] obs2 = this.getL0Observation("HELLO_FROM_ATTACKER");
+		String[] obs2 = this.getL0Observation("ACTION_FROM_ATTACKER");
 		LOGGER.debug("Attacker got observation " + Arrays.toString(obs2));
 		
 		return currentNode + 1;
